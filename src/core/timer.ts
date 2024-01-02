654466e06e3ac2a47b44ccccd590363081e7754c
Tomasz Wilczyński <twilczynski@naver.com>
2024-01-02 16:59:09 +0100

    src/core/timer: Add guards around updateAllRecurrences
    In order to make the plugin compliant with Joplin's plugin throttling
    mechanism, add guards around updateAllRecurrences in the timer.
    
    Ref: https://discourse.joplinapp.org/t/plugin-repeating-to-dos-maintainers-needed/16470/55
    
    Signed-off-by: Tomasz Wilczyński <twilczynski@naver.com>

--- src/core/timer.ts
+++ src/core/timer.ts
@@ -1,9 +1,15 @@
 import joplin from "api";
 import { updateAllRecurrences } from "./recurrence";
 
+var updating = null
 var timer = null
 
 export async function setupTimer(){
     clearInterval(timer)
-    timer = setInterval(updateAllRecurrences, await joplin.settings.value("updateFrequency") * 1000);
+    timer = setInterval(async () => {
+        if (updating) return;
+        updating = true;
+        await updateAllRecurrences();
+        updating = false;
+    }, await joplin.settings.value("updateFrequency") * 1000);
 }
